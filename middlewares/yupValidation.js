c2229a7be9c45ebbcb99f4dc89946dc7b4f328d3
HungNG2k2 <hung200220@gmail.com>
2023-10-12 12:17:14 +0700

    add auth validations

---
+++ middlewares/yupValidation.js
@@ -0,0 +1,13 @@
+const validate = (schema) => async (req, res, next) => {
+  try {
+    await schema.validate({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default validate;
