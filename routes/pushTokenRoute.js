dbd0dfa2389f6808bad55b5c485ec0425f34c7cf
HungNG2k2 <hung200220@gmail.com>
2024-01-01 22:29:34 +0700

    fix pushTokenRoute

--- routes/pushTokenRoute.js
+++ routes/pushTokenRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { checkRole } from '../middlewares/authorization.js';
+import { auth, checkRole } from '../middlewares/authorization.js';
 import { roles } from '../config/constant.js';
 import pushTokenController from '../controllers/pushTokenController.js';
 
 const router = express.Router();
 
-router.post('', checkRole([roles.EXPERT, roles.USER]), pushTokenController.saveToken);
+router.post('', auth, checkRole([roles.EXPERT, roles.USER]), pushTokenController.saveToken);
 
 export default router;
